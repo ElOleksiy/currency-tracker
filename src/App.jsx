9840dccf0b0af8c1a51f58ebdd5d6db6f4b109d3
Telenchak Oleksiy <letel315@gmail.com>
2023-12-15 00:15:06 +0200

    Add CurrecyCard logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,12 @@ import UserInput from "./components/UserInput";
 import Container from "./components/Container";
 import CurrencyCard from "./components/CurrencyCard";
 
-const tickers = [];
-
 function App() {
   const API_KEY =
     "9a2e01e570c0b693006d832c67ecc7a80ffbb3ed27cfd73218a1c37395c2584c";
 
   const [inputValue, setInputValue] = useState("");
+  const [tickers, setTickers] = useState([]);
 
   function handleButton() {
     fetch(
@@ -18,14 +17,18 @@ function App() {
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-        if (!data["Response"]) {
-          tickers.push({
-            name: inputValue,
-            price: data["USD"],
-          });
-        }
+        if (!(data["Response"] === "Error"))
+          setTickers((prevTickers) => [
+            ...prevTickers,
+            {
+              name: inputValue,
+              price: data["USD"],
+            },
+          ]);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
       });
-
     console.log(tickers);
     setInputValue("");
   }

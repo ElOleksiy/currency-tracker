9595cf8f1913475058d913a37d9de6cc881ef0c1
Telenchak Oleksiy <letel315@gmail.com>
2023-12-14 23:23:35 +0200

    add CurencyCard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import UserInput from "./components/UserInput";
 import Container from "./components/Container";
+import CurrencyCard from "./components/CurrencyCard";
+
+const tickers = [];
 
 function App() {
   const API_KEY =
     "9a2e01e570c0b693006d832c67ecc7a80ffbb3ed27cfd73218a1c37395c2584c";
 
   const [inputValue, setInputValue] = useState("");
-  const [tickers, setTickers] = useState([]);
 
   function handleButton() {
     fetch(
@@ -36,6 +38,15 @@ function App() {
           inputValue={inputValue}
           setInputValue={setInputValue}
         />
+        {tickers.map((ticker) => {
+          return (
+            <CurrencyCard
+              key={ticker.name}
+              currencyName={ticker.name}
+              price={ticker.price}
+            />
+          );
+        })}
       </Container>
     </>
   );
--- src/components/CurrencyCard.jsx
+++ src/components/CurrencyCard.jsx
@@ -1,5 +1,8 @@
 export default function CurrencyCard({ currencyName, price }) {
   return (
-
-  )
+    <>
+      <h4>{currencyName}</h4>
+      <p>{price}</p>
+    </>
+  );
 }

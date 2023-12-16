bbfff9dbdc4ef98b82dfcdfa4571ac8c51a6088e
Telenchak Oleksiy <letel315@gmail.com>
2023-12-17 00:26:03 +0200

    set up tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -64,26 +64,37 @@ function App() {
     localStorage.setItem("tickers", JSON.stringify(updatedTickers));
   }
 
+  const validTickers = () => {
+    return tickers.length;
+  };
+
   return (
-    <>
-      <Container>
+    <div className="container mx-auto flex flex-col items-center bg-gray-100 p-4">
+      <div className="container">
         <UserInput
+          setInputValue={setInputValue}
           handleButton={handleButton}
           inputValue={inputValue}
-          setInputValue={setInputValue}
         />
-        {tickers.map((ticker) => {
-          return (
-            <CurrencyCard
-              key={ticker.name}
-              currencyName={ticker.name}
-              price={ticker.price}
-              deleteCard={() => deleteCard(ticker.name)}
-            />
-          );
-        })}
-      </Container>
-    </>
+
+        {validTickers && (
+          <hr className="w-full border-t border-gray-600 my-4" />
+        )}
+        <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+          {tickers.map((ticker) => {
+            return (
+              <CurrencyCard
+                key={ticker.name}
+                currencyName={ticker.name}
+                price={ticker.price}
+                deleteCard={() => deleteCard(ticker.name)}
+              />
+            );
+          })}
+        </dl>
+        <hr className="w-full border-t border-gray-600 my-4" />
+      </div>
+    </div>
   );
 }
 

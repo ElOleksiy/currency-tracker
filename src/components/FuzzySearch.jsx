f982291c1469519bcaf757d4738c60d27880720d
Telenchak Oleksiy <letel315@gmail.com>
2023-12-18 00:23:46 +0200

    change logo

---
+++ src/components/FuzzySearch.jsx
@@ -0,0 +1,18 @@
+export default function FuzzySearch() {
+  return (
+    <div className="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap">
+      <span className="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+        BTC
+      </span>
+      <span className="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+        DOGE
+      </span>
+      <span className="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+        BCH
+      </span>
+      <span className="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer">
+        CHD
+      </span>
+    </div>
+  );
+}
